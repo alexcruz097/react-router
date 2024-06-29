0293342cee01b7ff4629ca5e11b1e8a202bca3e1
alexcruz097 <alexc017@yahoo.com>
2024-06-29 00:23:20 -0700

    added style

--- react-router-dogfinder/src/AddColor.jsx
+++ react-router-dogfinder/src/AddColor.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 const AddColor = (props) => {
   console.log(props.colors);
-  return <div>AddColor</div>;
+  return (
+    <>
+      <div className="add-color-html">
+        <div className="add-color-container">
+          <label htmlFor="color">Color Name</label>
+          <input type="text" id="color" />
+          <button>Add Color</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddColor;
